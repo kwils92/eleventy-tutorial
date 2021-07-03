dc437ff2ec036f7eb18aeb288f486a5c22972b65
Ken Wilson <kennethwilson92@hotmail.com>
2021-07-03 13:27:28 -0400

    init commit

---
+++ .eleventy.js
@@ -0,0 +1,7 @@
+module.exports = function(eleventyConfig) {
+    //This specifies this folder should be passed THROUGh the output 11ty is producing 
+    //No processing steps involved. Just the content 
+    eleventyConfig.addPassthroughCopy("images");
+    eleventyConfig.addPassthroughCopy("admin");
+    
+}
---
+++ posts/my-first-post.md
@@ -0,0 +1,11 @@
+---
+layout: layouts/post.njk
+title: My Post
+description: My description
+date: 2021-07-04
+featuredImage: /images/uploads/image1.jpg
+---
+
+Hello my post 
+
+
