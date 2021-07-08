c77641862c1b1284dcd682cedf691891a59da8cd
Ken Wilson <kennethwilson92@hotmail.com>
2021-07-07 23:28:28 -0400

    test to see how heavy font-awesome and the fonts are

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,13 @@
 module.exports = function(eleventyConfig) {
     //This specifies this folder should be passed THROUGh the output 11ty is producing 
     //No processing steps involved. Just the content 
-    eleventyConfig.addPassthroughCopy("images");
-    eleventyConfig.addPassthroughCopy("admin");
+    eleventyConfig.addPassthroughCopy("images")
+    eleventyConfig.addPassthroughCopy("admin")
+    eleventyConfig.addPassthroughCopy("assets")
+    eleventyConfig.addPassthroughCopy("css")
+    return {
+        passthroughFileCopy: true
+    }
+
     
 }
