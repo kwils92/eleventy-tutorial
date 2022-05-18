d69cfef5622a142332dbcb7cb1fb3998545b922b
Ken Wilson <kennethwilson92@hotmail.com>
2022-05-18 13:52:02 -0400

    Trying new way to validate captcha. Last chance...

--- _site/assets/js/unlock-submit.js
+++ _site/assets/js/unlock-submit.js
@@ -1,6 +1,11 @@
-function recaptchaCallBack(){
-    let btn = document.getElementById("contact-submit");
-    btn.removeAttribute('disabled');
-
-}
-
+window.onload = function() {
+    //check to make sure form is on page. 
+    if(typeof document.forms["contact"] !== 'undefined'){
+        var recaptcha = document.forms["contact"]["g-recaptcha-response"];
+        recaptcha.required = true;
+        recaptcha.oninvalid = function(e) {
+            let btn = document.getElementById("contact-submit");
+            btn.removeAttribute('disabled');        
+        }
+    } 
+  }
